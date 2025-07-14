9bd92ab979e0c8008868b17c50ddaba3ce392d2b
leicharles <leicharles22@gmail.com>
2025-07-14 11:56:10 +0800

    hello

--- Concert Reservation App/Form1.cs
+++ Concert Reservation App/Form1.cs
@@ -16,5 +16,10 @@ namespace Concert_Reservation_App
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

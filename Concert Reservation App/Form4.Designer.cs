6ea7bfc13814ae2d178ed197f3e6e86cd8498adc
Maricsss <mrcrpanganiban2004@gmail.com>
2025-07-14 13:54:43 +0800

    done

--- Concert Reservation App/Form4.Designer.cs
+++ Concert Reservation App/Form4.Designer.cs
@@ -28,26 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblArtistName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.lblVenue = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSelectSeat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblArtistName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblArtistName.AutoSize = true;
+            this.lblArtistName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArtistName.Location = new System.Drawing.Point(118, 45);
+            this.lblArtistName.Name = "lblArtistName";
+            this.lblArtistName.Size = new System.Drawing.Size(165, 25);
+            this.lblArtistName.TabIndex = 0;
+            this.lblArtistName.Text = "BEN AND BEN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(157, 84);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(70, 13);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "July 18, 2025";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(167, 117);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(47, 13);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "3:00 PM";
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.Location = new System.Drawing.Point(154, 147);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(71, 13);
+            this.lblPlace.TabIndex = 4;
+            this.lblPlace.Text = "MOA ARENA";
+            // 
+            // lblVenue
+            // 
+            this.lblVenue.AutoSize = true;
+            this.lblVenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVenue.Location = new System.Drawing.Point(87, 206);
+            this.lblVenue.Name = "lblVenue";
+            this.lblVenue.Size = new System.Drawing.Size(81, 24);
+            this.lblVenue.TabIndex = 5;
+            this.lblVenue.Text = "VENUE:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(91, 247);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 290);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnSelectSeat
+            // 
+            this.btnSelectSeat.Location = new System.Drawing.Point(107, 555);
+            this.btnSelectSeat.Name = "btnSelectSeat";
+            this.btnSelectSeat.Size = new System.Drawing.Size(198, 37);
+            this.btnSelectSeat.TabIndex = 7;
+            this.btnSelectSeat.Text = "SELECT YOUR DESIRED SEAT";
+            this.btnSelectSeat.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 698);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectSeat);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblVenue);
+            this.Controls.Add(this.lblPlace);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblArtistName);
             this.Name = "Form4";
             this.Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +135,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblArtistName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.Label lblVenue;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSelectSeat;
     }
 }
--- Concert Reservation App/Form4.cs
+++ Concert Reservation App/Form4.cs
@@ -16,5 +16,10 @@ namespace Concert_Reservation_App
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

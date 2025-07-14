8cd85f05e9c1c719a9567122466cca4743fd572f
Maricsss <mrcrpanganiban2004@gmail.com>
2025-07-14 16:02:52 +0800

    newwww

--- Concert Reservation App/Form12.Designer.cs
+++ Concert Reservation App/Form12.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form12
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form12";
+            this.ClientSize = new System.Drawing.Size(293, 522);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form12";
+            this.Text = "Form11";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
